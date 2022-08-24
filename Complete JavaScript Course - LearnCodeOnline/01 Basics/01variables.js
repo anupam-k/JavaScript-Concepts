a8c9691318f99160552d2e1c4f3385ab552fd4b1
Anupam Kumar Krishnan <91872149+anupam-k@users.noreply.github.com>
2022-08-24 18:49:18 +0530

    Create 01variables.js

---
+++ Complete JavaScript Course - LearnCodeOnline/01 Basics/01variables.js
@@ -0,0 +1,31 @@
+//reserve some memory
+// var, let and const
+var fullName = "Anupam Kumar Krishnan";
+
+// how to name variables
+
+// fullName
+// full-name
+// full_name
+// FullName
+
+var courseName = "JavaScript Web Development Bootcamp";
+
+var isLoggedIn = true;
+// false is reserved keywords
+
+var loggedCount = 34;
+console.log(loggedCount);
+console.log("loggedCount");
+
+var paymentMode;
+console.log(paymentMode);
+
+paymentMode = "Credit Card";
+
+var a = 20;
+if (a == 9) {
+  console.log("true");
+} else {
+  console.log("false");
+}
