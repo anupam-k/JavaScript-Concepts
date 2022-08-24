c85de43388483bc473a409cd2920bdafe46b1884
Anupam Kumar Krishnan <91872149+anupam-k@users.noreply.github.com>
2022-08-24 18:52:26 +0530

    Conditionals

---
+++ Complete JavaScript Course - LearnCodeOnline/01 Basics/04conditionals.js
@@ -0,0 +1,18 @@
+var temperature;
+
+//TODO: Go to gooogle and get the data
+
+temperature = 59;
+
+//var result = temperature < 20;
+//console.log(result);
+
+if (temperature === 20) {
+  console.log("It's very cold outside");
+}
+
+if (temperature < 30) {
+  console.log("It's moderate outside");
+} else {
+  console.log("It's really hot outside");
+}
