4ab2109a258ba87b44fab80616b0998eeac361b1
Anupam Kumar Krishnan <91872149+anupam-k@users.noreply.github.com>
2022-08-24 18:57:09 +0530

    Context ✅

---
+++ Complete JavaScript Course - LearnCodeOnline/02Intermediate/03context.js
@@ -0,0 +1,15 @@
+// sayHello();
+
+// function sayHello() {
+//   console.log("Hello");
+// }
+
+if (2 === "2") {
+  console.log("this is true!");
+}
+
+var myName = "hitesh";
+
+if (myName === window.myName) {
+  console.log("This is again a true Statement");
+}
