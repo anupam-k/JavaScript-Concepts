d22f607b0280264dede61421f583fd51792b4b57
Anupam Kumar Krishnan <91872149+anupam-k@users.noreply.github.com>
2022-08-24 18:52:03 +0530

    Operators

---
+++ Complete JavaScript Course - LearnCodeOnline/01 Basics/03operators.js
@@ -0,0 +1,21 @@
+// Operators: +, -, /, %, *
+
+// var num1 = 7;
+// var num2 = 6;
+
+// console.log("Addition: " + (num1 + num2));
+// console.log("Substraction: " + (num1 - num2));
+// console.log("Multiplication: " + num1 * num2);
+// console.log("Division: " + num1 / num2);
+// console.log("Modulous: " + (num1 % num2));
+// var answer = num1 > num2;
+// console.log("Answer: " + answer);
+
+var listingPrice = 1100;
+var sellingPrice = 800;
+var discountPercentage = ((listingPrice - sellingPrice) / listingPrice) * 100;
+console.log("Discount Percentage: " + discountPercentage + " %");
+displayDiscountPercentage = Math.round(discountPercentage);
+console.log("Display Discount Percentage: " + displayDiscountPercentage);
+
+//Operator Precedence
